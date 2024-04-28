d32335ba1d32b244edcfd543593cff7baa69fdde
utkarsh <uutkarsh1234@gmail.com>
2024-04-28 16:12:42 +0530

    1st commit

---
+++ app/src/main/java/com/example/compressive_strength_calculator/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.compressive_strength_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public static void cal_buton(){
+
+    }
+}
